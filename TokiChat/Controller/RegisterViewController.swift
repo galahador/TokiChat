464fd6d6304063040f020a15f7182f127e3c4049
Petar Lemajic <galahador@gmail.com>
2018-08-27 16:32:03 +0200

    Update

---
+++ TokiChat/Controller/RegisterViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterViewController.swift
+//  TokiChat
+//
+//  Created by Petar Lemajic on 8/24/18.
+//  Copyright © 2018 Metalic_archaea. All rights reserved.
+//
+
+import Foundation
---
+++ TokiChat/CustomCell/CustomCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCellTableViewCell.swift
+//  TokiChat
+//
+//  Created by Petar Lemajic on 8/24/18.
+//  Copyright © 2018 Metalic_archaea. All rights reserved.
+//
+
+import UIKit
+
+class CustomCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
